e2b99f22d3646817948dc38e4cb6f0ff3bc4526a
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-06-30 14:22:29 +0100

    Create InventaireForm.page.dart

---
+++ lib/pages/InventaireForm.page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class InventaireForm extends StatefulWidget {
+  const InventaireForm({Key? key}) : super(key: key);
+
+  @override
+  State<InventaireForm> createState() => _InventaireFormState();
+}
+
+class _InventaireFormState extends State<InventaireForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
