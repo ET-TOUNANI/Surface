986ab7e397a4608a7063544989d71dec44be9173
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-06-30 14:39:14 +0100

    Update Dashboard.page.dart

--- lib/pages/Dashboard.page.dart
+++ lib/pages/Dashboard.page.dart
@@ -35,22 +35,27 @@ class Dashboard extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        _getSizedBox(context,"scan", "Inventaire", 1,"/inventer"),
-                        _getSizedBox(context,"importer", "Importer", 2,"/importer"),
+                        _getSizedBox(context,"scan","Inventaire",1,"/inventer"),
+
+                        _getSizedBox(
+                            context, "importer", "Importer", 2, "/importer"),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        _getSizedBox(context,"todo", "Lieu", 3,"/lieu"),
-                        _getSizedBox(context,"settings", "Famille", 4,"/famille"),
+                        _getSizedBox(context, "todo", "Lieu", 3, "/lieu"),
+                        _getSizedBox(
+                            context, "settings", "Famille", 4, "/famille"),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        _getSizedBox(context,"calendar", "Exporter", 5,"/exporter"),
-                        _getSizedBox(context,"note", "Réinitialiser", 6,"/reinitialiser"),
+                        _getSizedBox(
+                            context, "calendar", "Exporter", 5, "/exporter"),
+                        _getSizedBox(context, "note", "Réinitialiser", 6,
+                            "/reinitialiser"),
                       ],
                     ),
                   ],
@@ -63,50 +68,55 @@ class Dashboard extends StatelessWidget {
 }
 
 //To avoid re-writing the same code in the menu buttons.
-GestureDetector _getSizedBox(BuildContext context,image /*the image of the button*/,
-    role /*the function of the button*/, number /*the number of the button*/,route /* what we will do when the button clicked */) {
-  return GestureDetector(
-    behavior: HitTestBehavior.translucent,
-    onTap: (){
-      Navigator.push(context, route);
-    },
-    child: SizedBox(
-      width: 160.0,
-      height: 160.0,
-      child: Card(
-        color: const Color.fromARGB(255, 21, 21, 21),
-        elevation: 2.0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-        child: Center(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                "assets/$image.png",
-                width: 64.0,
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                "$role",
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0),
-              ),
-              const SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                "$number",
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.w100),
-              )
-            ],
-          ),
-        )),
+_getSizedBox(
+    BuildContext context,
+    image /*the image of the button*/,
+    role /*the function of the button*/,
+    number /*the number of the button*/,
+    route /* what we will do when the button clicked */
+    ) {
+  return SizedBox(
+    width: 154.0,
+    height: 160.0,
+    child: Material(
+      color: const Color.fromARGB(255, 21, 21, 21),
+      elevation: 8,
+      borderRadius: BorderRadius.circular(10),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: InkWell(
+        splashColor: Colors.black26,
+        onTap: () {
+          Navigator.pushNamed(context, "$route");
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Ink.image(
+              image:  AssetImage('assets/$image.png'),
+              height: 80,
+              width: 64.0,
+              //fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "$role",
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              "$number",
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w800),
+            )
+          ],
+        ),
       ),
     ),
   );
