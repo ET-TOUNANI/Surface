78ae80d29fc76f039037ce28fa88fb0fbbdd0bb7
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-07-12 20:04:14 +0100

    Create ResultCard.page.dart

---
+++ lib/pages/inventer/ResultCard.page.dart
@@ -0,0 +1,19 @@
+import 'package:exemple1/configs/AppBar.config.dart';
+import 'package:flutter/material.dart';
+import 'package:exemple1/configs/GetButtonNavigatBar.config.dart';
+
+class ResultCard extends StatelessWidget {
+  const ResultCard({super.key, required this.resWidget});
+  final Widget resWidget;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: GetAppBare(),
+      body: SingleChildScrollView(
+        child: resWidget, // show the card details of the immo
+      ),
+      bottomNavigationBar: GetButtonNavigatBar(context),
+    );
+  }
+}
