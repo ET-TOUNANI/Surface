b1619d2a2960af2c6c3a68973c9097deac279bd1
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-07-19 23:27:25 +0100

    Create pdf.widget.dart

---
+++ lib/widgets/pdf.widget.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class PDFApi {
+  static Future<File> loadAsset(String path) async {
+    final data = await rootBundle.load(path);
+    final bytes = data.buffer.asUint8List();
+
+    return _storeFile(path, bytes);
+  }
+  static Future<File> _storeFile(String url, List<int> bytes) async {
+    final filename = basename(url);
+    final dir = await getApplicationDocumentsDirectory();
+
+    final file = File('${dir.path}/$filename');
+    await file.writeAsBytes(bytes, flush: true);
+    return file;
+  }
+}
