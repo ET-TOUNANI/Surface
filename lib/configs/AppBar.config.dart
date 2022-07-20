0b9975583306c242874d38b5c84142baa5a784d0
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-07-20 17:30:12 +0100

    Update AppBar.config.dart

--- lib/configs/AppBar.config.dart
+++ lib/configs/AppBar.config.dart
@@ -3,10 +3,45 @@ import 'package:flutter/material.dart';
 AppBar GetAppBare() {
   return AppBar(
     backgroundColor: const Color.fromARGB(255, 33, 33, 33),
-    title: const Text(
-      "Surface",
-      style: TextStyle(
-          color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+    title:  Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text(
+          "S",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "U",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "R",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "F",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "A",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "C",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "E",
+          style: TextStyle(
+              color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+      ],
     ),
   );
 }
