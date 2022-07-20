1345256ea4b814151d91c7d083acc5e9e1cfbffe
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-07-20 17:20:02 +0100

    Update AppBar.config.dart

--- lib/configs/AppBar.config.dart
+++ lib/configs/AppBar.config.dart
@@ -4,9 +4,10 @@ AppBar GetAppBare() {
   return AppBar(
     backgroundColor: const Color.fromARGB(255, 33, 33, 33),
     title: const Text(
-      "App name",
+      "Surface",
       style: TextStyle(
-          color: Colors.white, fontSize: 28.0, fontWeight: FontWeight.bold),
+          color: Color.fromARGB(255,0, 118, 182), fontSize: 28.0, fontWeight: FontWeight.bold),
     ),
   );
 }
+//https://onlinepngtools.com/change-png-color
