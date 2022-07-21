fc3e093fd13910c389aae8e37df02bf84d5f1b29
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-07-21 16:01:47 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
+
+/*
+                         احسن مبرمجين في العالم
+
+                         ----------ABDERRAHMANE ETTOUNANI----------
+                         | Gmail : tounaniabderrahmane99@gmail.com |
+                         | TEL   : +212 6 89 97 18 37              |
+
+                         |----------HAMID BOUARGALNE----------
+                         | Gmail : bouargalne.hamid@gmail.com
+                         | TEL   : +212 6 49 94 91 59
+
+                         تحت تاطير السيد ياسين رضى
+ */
+
+
+
 import 'package:exemple1/pages/Dashboard.page.dart';
 import 'package:exemple1/pages/Exporte.page.dart';
 import 'package:exemple1/pages/inventer/InventaireForm.page.dart';
